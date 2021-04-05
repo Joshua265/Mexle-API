ad97e455b1ae3d6be44cc355c94b27ea51a711dd
Joshua265 <Joshua_Noel@gmx.de>
2021-04-05 14:52:21 +0200

    add access-control-allow-origin header

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://kurse.mexle.org');
   res.header(
     'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Origin',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
