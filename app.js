07887ba35552d7fd9827cea1b7ba7e1e156fab1f
Joshua265 <Joshua_Noel@gmx.de>
2021-04-05 13:56:51 +0200

    allow content type json

--- app.js
+++ app.js
@@ -1,56 +1,53 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const helmet = require("helmet");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
 
-require("dotenv/config");
+require('dotenv/config');
 
 // use cors
 const corsOptions = {
-  origin: "https://kurse.mexle.org",
+  origin: 'https://kurse.mexle.org',
   optionsSuccessStatus: 200, // For legacy browser support
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 app.use(cors(corsOptions));
 
-//use helmet to disable x-frame header
-app.use(
-  helmet.frameguard({
-    action: "deny",
-  })
-);
+//use helmet
+app.use(helmet());
 
 //better logging
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 //connect to db
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("connected to DB");
+    console.log('connected to DB');
   }
 );
 
 //import routes
-const userRoute = require("./routes/user");
-const coursesRoute = require("./routes/courses");
-const chaptersRoute = require("./routes/chapters");
-const stepsRoute = require("./routes/steps");
-const imagesRoute = require("./routes/images");
+const userRoute = require('./routes/user');
+const coursesRoute = require('./routes/courses');
+const chaptersRoute = require('./routes/chapters');
+const stepsRoute = require('./routes/steps');
+const imagesRoute = require('./routes/images');
 
 //use routes
-app.use("/user", userRoute);
-app.use("/courses", coursesRoute);
-app.use("/chapters", chaptersRoute);
-app.use("/steps", stepsRoute);
-app.use("/images", imagesRoute);
+app.use('/user', userRoute);
+app.use('/courses', coursesRoute);
+app.use('/chapters', chaptersRoute);
+app.use('/steps', stepsRoute);
+app.use('/images', imagesRoute);
 
 //home route
-app.get("/", (req, res) => {
-  console.log("Responding to root route");
-  res.send("<h1>Welcome to the Mexle API Server!</h1>");
+app.get('/', (req, res) => {
+  console.log('Responding to root route');
+  res.send('<h1>Welcome to the Mexle API Server!</h1>');
 });
 
 //listen on PORT
